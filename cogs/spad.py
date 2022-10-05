311aeb8ff277f0886d2c015ccd72e4c8fae155fe
Parth Jai <42300701+ParthJai@users.noreply.github.com>
2022-10-05 20:51:21 +0530

    Update spad.py

--- cogs/spad.py
+++ cogs/spad.py
@@ -3,6 +3,7 @@ import requests
 import pdfplumber
 import interactions
 import config
+import datetime
 
 url = 'https://www.marscna.org/wp-content/uploads/2022/01/SPAD-ApprovalDraft_Jan22_1_WEB.pdf'
 
@@ -18,18 +19,26 @@ class spad(interactions.Extension):
 			name='page',
 			description='enter page no. of pdf (not book)',
 			type=interactions.OptionType.INTEGER,
-			required=True,
 			min_value = 0,
 			max_value = 392,
 			),
 		],
 	)
 
-	async def ext_command(self, ctx:interactions.CommandContext, page: int):
+	async def ext_command(self, ctx:interactions.CommandContext, page: int = None):
 		await ctx.defer(ephemeral=False)
 		r = requests.get(url)
 		with pdfplumber.open(BytesIO(r.content)) as pdf:
-			extracted_page = pdf.pages[page-1].extract_text()
+			if page != None:
+				extracted_page = pdf.pages[page-1].extract_text()
+			else:
+				current_day = datetime.datetime.now().timetuple().tm_yday
+				current_month = datetime.date.today().month
+				page = current_day + current_month + 7
+				year = datetime.date.today().year
+				if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+					page -= 1
+				extracted_page = pdf.pages[page-1].extract_text()
 		chunklength = 1900
 		chunks = [extracted_page[i:i+chunklength] for i in range (0, len(extracted_page), chunklength)]
 		for chunk in chunks:
