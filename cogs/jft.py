d37f64902b9ba85e159373a2af15c29494fbb645
Parth Jai <42300701+ParthJai@users.noreply.github.com>
2022-10-06 20:24:05 +0530

    async requests

---
+++ cogs/jft.py
@@ -0,0 +1,37 @@
+import interactions
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+url = 'https://www.jftna.org/jft/'
+
+class jft(interactions.Extension):
+    def __init__(self, client: interactions.Client):
+        self.client: interactions.Client = client
+
+    def scrape(self, r):
+        s = BeautifulSoup(r, 'html.parser')
+        text = "```"
+        for tag in s.find_all('tr'):
+            text += tag.text
+            text += '\n'
+        text += "```"
+        return text
+
+    @interactions.extension_command(
+        name="jft",
+        description="For daily readings",
+    )
+
+    
+    async def ext_command(self, ctx: interactions.CommandContext):
+        loop = asyncio.get_running_loop()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                r = await response.text()
+                text = await loop.run_in_executor(None, self.scrape, r)
+                await ctx.send(text)
+
+
+def setup(client):
+    jft(client)
